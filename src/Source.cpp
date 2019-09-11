52b6b8945b565f24be9192decf40dfdaf12fe510
julianbega <julian.begamilrad@gmail.com>
2019-09-11 10:55:09 -0300

    modified:   src/Source.cpp

--- src/Source.cpp
+++ src/Source.cpp
@@ -7,20 +7,13 @@ enum Colors { Red = 1, Blue, Green, Yellow};
 const int screenWidth = 800;
 const int screenHeight = 450;
 
-bool CanHitP1 = false;
-bool PlayPVP = false;
-bool PlayPVE = false;
-bool Play = false;
-bool Victory = false;
-bool P1Win = false;
-int speed = 3;
+
 struct Vector2
 {
 	int x;
 	int y;
 	
 };
-
 struct Rectangle
 {
 	Vector2 Pos;
@@ -28,7 +21,6 @@ struct Rectangle
 	float height;
 
 } ;
-
 struct Player
 {
 	Rectangle Rec;
@@ -36,8 +28,7 @@ struct Player
 	int Puntos;
 	int TotalScore;
 
-}Player1, Player2;
-
+} ;
 struct Ball
 {
 	Vector2 Position;
@@ -47,23 +38,28 @@ struct Ball
 	bool Arriba;
 	bool Derecha;
 }Pelota;
-
-
+#pragma region Declaracion de funciones
 void TextColor(int ColorName);
-void Menu();
-void PVP();
-void PVE();
-void Init();
-void GameOver(int PW);
+void Menu(Player& Player1, Player& Player2);
+void PVP(Player& Player1, Player& Player2);
+void PVE(Player& Player1, Player& Player2);
+void Init(Player& Player1, Player& Player2);
+Player InitPlayer(int posX, int posY, int width, int height);
+void GameOver(int PW, Player& Player1, Player& Player2);
+void MovePlayer(Player& Player, int KeyUp, int KeyDown);
+void BallMovment (Ball& Pelota);
+void BallMovmentLimit(Ball& Pelota, Player& Player1, Player& Player2);
+void BallColisionWithPlayers(Ball& Pelota, Player& Player1, Player& Player2);
+void Draw(Ball& Pelota, Player& Player1, Player& Player2);
+#pragma endregion
 
 void main()
 {
-	Init();
-	Menu();
-
-
+	Player Player1 = InitPlayer(40, 80, 20, 80);
+	Player Player2 = InitPlayer(screenWidth - 40, 80, 20, 80);
+	Init(Player1, Player2);
+	Menu(Player1, Player2);
 }
-
 void TextColor(int ColorName)
 {
 	switch (ColorName) {
@@ -87,122 +83,89 @@ void TextColor(int ColorName)
 		break;
 	}
 }
-
-void PVP()
+Player InitPlayer(int posX, int posY, int width, int height)
+{
+	Player player;
+	player.Rec.Pos.x = posX;
+	player.Rec.Pos.y = posY;
+	player.Rec.width = width;
+	player.Rec.height = height;
+	player.Puntos = 0;
+	player.TotalScore = 0;
+	return player;
+}
+void PVP(Player& Player1, Player& Player2)
 {
 	while (!slShouldClose())
 	{
-		//// CONTROLS
-		{
-			if (slGetKey('W') && Player1.Rec.Pos.y <= screenHeight - Player1.Rec.height / 2)
-			{
-				Player1.Rec.Pos.y = Player1.Rec.Pos.y + speed;
-			}
-			if (slGetKey('S') && Player1.Rec.Pos.y >= 0 + Player1.Rec.height / 2)
-			{
-				Player1.Rec.Pos.y = Player1.Rec.Pos.y - speed;
-			}
-
-			if (slGetKey(SL_KEY_UP) && Player2.Rec.Pos.y <= screenHeight - Player2.Rec.height / 2)
-			{
-				Player2.Rec.Pos.y = Player2.Rec.Pos.y + speed;
-			}
-			if (slGetKey(SL_KEY_DOWN) && Player2.Rec.Pos.y >= 0 + Player2.Rec.height / 2)
-			{
-				Player2.Rec.Pos.y = Player2.Rec.Pos.y - speed;
-			}
-
-		}
-
-		//// MOVIMIENTOS PELOTA
-		{
-			if (Pelota.Arriba == true)
-			{
-				if (Pelota.Derecha == true)
-				{
-					Pelota.Position.x = Pelota.Position.x + Pelota.StandarSpeed;
-					Pelota.Position.y = Pelota.Position.y + Pelota.StandarSpeed;
-				}
-				else
-				{
-					Pelota.Position.x = Pelota.Position.x - Pelota.StandarSpeed;
-					Pelota.Position.y = Pelota.Position.y + Pelota.StandarSpeed;
-				}
-			}
-			else
-			{
-				if (Pelota.Derecha == true)
-				{
-					Pelota.Position.x = Pelota.Position.x + Pelota.StandarSpeed;
-					Pelota.Position.y = Pelota.Position.y - Pelota.StandarSpeed;
-				}
-				else
-				{
-					Pelota.Position.x = Pelota.Position.x - Pelota.StandarSpeed;
-					Pelota.Position.y = Pelota.Position.y - Pelota.StandarSpeed;
-				}
-			}
-		}
-
-		//// COLISION PELOTA LIMITES
-		{
-			if (Pelota.Position.y <= 0 + Pelota.Radius) { Pelota.Arriba = true; }
-			if (Pelota.Position.y >= screenHeight - Pelota.Radius) { Pelota.Arriba = false; }
-			if (Pelota.Position.x <= 0 + Pelota.Radius) { Pelota.Derecha = true; Player2.Puntos++; }
-			if (Pelota.Position.x >= screenWidth - Pelota.Radius) { Pelota.Derecha = false; Player1.Puntos++; }
-		}
-
-		//// COLISIONES PALETAS PELOTA
-		{
-			if (Pelota.Position.x <= Player1.Rec.Pos.x + Player1.Rec.width / 2 + Pelota.Radius && Pelota.Position.y <= Player1.Rec.Pos.y + Player1.Rec.height / 2 && Pelota.Position.y >= Player1.Rec.Pos.y - Player1.Rec.height / 2)
-			{
-				Pelota.Derecha = true;
-			}
-			if (Pelota.Position.x >= Player2.Rec.Pos.x - Player2.Rec.width / 2 - Pelota.Radius && Pelota.Position.y <= Player2.Rec.Pos.y + Player2.Rec.height / 2 && Pelota.Position.y >= Player2.Rec.Pos.y - Player2.Rec.height / 2)
-			{
-				Pelota.Derecha = false;
-			}
-		}
-
-
-		slSetFontSize(11);
-		TextColor(Red);
-		slRectangleFill(Player1.Rec.Pos.x, Player1.Rec.Pos.y, Player1.Rec.width, Player1.Rec.height);
-		TextColor(Blue);
-		slRectangleFill(Player2.Rec.Pos.x, Player2.Rec.Pos.y, Player2.Rec.width, Player2.Rec.height);
-		TextColor(Yellow);
-		slCircleFill(Pelota.Position.x, Pelota.Position.y, Pelota.Radius, Pelota.numVertices);
-
-		string PuntosP1 = (" Puntos Player 1: " + (to_string(Player1.Puntos)));
-		string PuntosP2 = (" Puntos Player 2: " + (to_string(Player2.Puntos)));
-		TextColor(Red);
-		slText(20, screenHeight - 20, PuntosP1.c_str());
-		TextColor(Blue);
-		slText(screenWidth - 180, screenHeight - 20, PuntosP2.c_str());
-		if (slGetKey(SL_KEY_ESCAPE))
-		{
-			Menu();
-		}
-		if (Player1.Puntos > 1) { GameOver(1); }
-		if(Player2.Puntos > 1) {GameOver(2);}
-	
-		slRender(); // draw everything
+		MovePlayer(Player1, 'W', 'S');
+		MovePlayer(Player2, SL_KEY_UP, SL_KEY_DOWN);
+		BallMovment(Pelota);
+		BallMovmentLimit(Pelota, Player1, Player2);
+		BallColisionWithPlayers(Pelota, Player1, Player2);
+		Draw(Pelota, Player1, Player2);
+		
 	}
 }
-
-void Init() {
-	Player1.Rec.Pos.x = 40;
-	Player1.Rec.Pos.y = 80;
-	Player1.Rec.width = 20;
-	Player1.Rec.height = 80;
-	Player2.Rec.Pos.x = screenWidth - 40;
-	Player2.Rec.Pos.y = 80;
-	Player2.Rec.width = 20;
-	Player2.Rec.height = 80;
-	Player1.Puntos = 0;
-	Player1.TotalScore = 0;
-	Player2.Puntos = 0;
-	Player2.TotalScore = 0;
+void BallColisionWithPlayers(Ball& Pelota, Player& Player1, Player& Player2)
+{
+	if (Pelota.Position.x <= Player1.Rec.Pos.x + Player1.Rec.width / 2 + Pelota.Radius && Pelota.Position.y <= Player1.Rec.Pos.y + Player1.Rec.height / 2 && Pelota.Position.y >= Player1.Rec.Pos.y - Player1.Rec.height / 2)
+	{
+		Pelota.Derecha = true;
+	}
+	if (Pelota.Position.x >= Player2.Rec.Pos.x - Player2.Rec.width / 2 - Pelota.Radius && Pelota.Position.y <= Player2.Rec.Pos.y + Player2.Rec.height / 2 && Pelota.Position.y >= Player2.Rec.Pos.y - Player2.Rec.height / 2)
+	{
+		Pelota.Derecha = false;
+	}
+}
+void MovePlayer(Player& Player,int KeyUp, int KeyDown) 
+{
+	if (slGetKey(KeyUp) && Player.Rec.Pos.y <= screenHeight - Player.Rec.height / 2)
+	{
+		Player.Rec.Pos.y = Player.Rec.Pos.y + 3;
+	}
+	if (slGetKey(KeyDown) && Player.Rec.Pos.y >= 0 + Player.Rec.height / 2)
+	{
+		Player.Rec.Pos.y = Player.Rec.Pos.y - 3;
+	}
+}
+void BallMovment(Ball& Pelota)
+{
+	if (Pelota.Arriba == true)
+	{
+		if (Pelota.Derecha == true)
+		{
+			Pelota.Position.x = Pelota.Position.x + Pelota.StandarSpeed;
+			Pelota.Position.y = Pelota.Position.y + Pelota.StandarSpeed;
+		}
+		else
+		{
+			Pelota.Position.x = Pelota.Position.x - Pelota.StandarSpeed;
+			Pelota.Position.y = Pelota.Position.y + Pelota.StandarSpeed;
+		}
+	}
+	else
+	{
+		if (Pelota.Derecha == true)
+		{
+			Pelota.Position.x = Pelota.Position.x + Pelota.StandarSpeed;
+			Pelota.Position.y = Pelota.Position.y - Pelota.StandarSpeed;
+		}
+		else
+		{
+			Pelota.Position.x = Pelota.Position.x - Pelota.StandarSpeed;
+			Pelota.Position.y = Pelota.Position.y - Pelota.StandarSpeed;
+		}
+	}
+}
+void BallMovmentLimit(Ball& Pelota, Player& Player1, Player& Player2)
+{
+	if (Pelota.Position.y <= 0 + Pelota.Radius) { Pelota.Arriba = true; }
+	if (Pelota.Position.y >= screenHeight - Pelota.Radius) { Pelota.Arriba = false; }
+	if (Pelota.Position.x <= 0 + Pelota.Radius) { Pelota.Derecha = true; Player2.Puntos++; }
+	if (Pelota.Position.x >= screenWidth - Pelota.Radius) { Pelota.Derecha = false; Player1.Puntos++; }
+}
+void Init(Player& Player1, Player& Player2) {
 	Pelota.Position.x = screenWidth / 2;
 	Pelota.Position.y = screenHeight / 2;
 	Pelota.Radius = 9;
@@ -215,7 +178,7 @@ void Init() {
 	slSetFont(slLoadFont("../Fonts/white_rabbit.ttf"), 24);
 	
 }
-void Menu() 
+void Menu(Player& Player1, Player& Player2)
 {
 	while (!slShouldClose())
 	{
@@ -229,14 +192,39 @@ void Menu()
 		}
 		if (slGetKey(SL_KEY_ENTER))
 		{
-			PVP();
+			PVP(Player1, Player2);
 		}
 		
 		slRender();
 	}
 }
+void Draw(Ball& Pelota, Player& Player1, Player& Player2)
+{
+	slSetFontSize(11);
+	TextColor(Red);
+	slRectangleFill(Player1.Rec.Pos.x, Player1.Rec.Pos.y, Player1.Rec.width, Player1.Rec.height);
+	TextColor(Blue);
+	slRectangleFill(Player2.Rec.Pos.x, Player2.Rec.Pos.y, Player2.Rec.width, Player2.Rec.height);
+	TextColor(Yellow);
+	slCircleFill(Pelota.Position.x, Pelota.Position.y, Pelota.Radius, Pelota.numVertices);
 
-void GameOver(int PW)
+	string PuntosP1 = (" Puntos Player 1: " + (to_string(Player1.Puntos)));
+	string PuntosP2 = (" Puntos Player 2: " + (to_string(Player2.Puntos)));
+	TextColor(Red);
+	slText(20, screenHeight - 20, PuntosP1.c_str());
+	TextColor(Blue);
+	slText(screenWidth - 180, screenHeight - 20, PuntosP2.c_str());
+	if (slGetKey(SL_KEY_ESCAPE))
+	{
+		Menu(Player1, Player2);
+	}
+	if (Player1.Puntos > 1) { GameOver(1, Player1, Player2); }
+	if (Player2.Puntos > 1) { GameOver(2, Player1, Player2); }
+
+	slRender(); // draw everything
+}
+
+void GameOver(int PW, Player& Player1, Player& Player2)
 {
 	while (!slShouldClose())
 	{
@@ -255,7 +243,7 @@ void GameOver(int PW)
 		}
 		if (slGetKey(SL_KEY_ESCAPE))
 		{
-			Menu();
+			Menu(Player1, Player2);
 		}
 		slRender();
 	}
